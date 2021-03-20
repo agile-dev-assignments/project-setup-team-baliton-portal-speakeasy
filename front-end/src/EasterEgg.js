670d0664f1c569f92cddaba6d6592664ea4a46de
Oli <oli@Olis-Macbook-Pro.local>
2021-03-20 12:47:00 +0000

    easterEggPage files added but unable to get Speakeasy button to swap to linker - need help

---
+++ front-end/src/EasterEgg.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './EasterEgg.css';
+// import Button from 'react-bootstrap/Button';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import background from "./front-page-background.png"
+
+
+const EasterEgg = () => {
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
+  
+    if (count === 3){
+      alert("3 reached!");
+    }
+    return (
+      <div className="App" >
+        
+        <header className="App-header" >
+        
+        <div className="background" style={{ backgroundImage:`url(${background})` }}>
+          <p>clicked {count} yes </p>
+            <button id="easterButton" onClick={() => setCount(count + 1)}></button>
+        </div>
+        
+       
+        </header>
+       
+      </div>
+    )
+  }
+
+  export default EasterEgg;
