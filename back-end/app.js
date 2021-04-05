cef201d688e80af10d4775c417a0fcf6cb680c74
Daniel Tomkovicz <dht253@nyu.edu>
2021-04-04 21:46:55 -0400

    semi colon

--- back-end/app.js
+++ back-end/app.js
@@ -91,4 +91,4 @@ app.get('/tagCallList/:id', (req, res, next)=>{
 
 // we will put some server logic here later...
 // export the express app we created to make it available to other modules
-module.exports = app;
+module.exports = app
