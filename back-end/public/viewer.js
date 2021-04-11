85f34d9bf070732e69d215046288f45de903ee22
Oli <oli@Olis-Macbook-Pro.local>
2021-04-11 15:55:01 +0100

    comments added to viewer.js

--- back-end/public/viewer.js
+++ back-end/public/viewer.js
@@ -4,12 +4,14 @@ window.onload = () => {
         init();
     }
 }
-
+//initialise peer and create a channel between server and user 
+// send video down channel
 async function init() {
     const peer = createPeer();
     peer.addTransceiver("video", { direction: "recvonly" })
 }
 
+// create peer object 
 function createPeer() {
     const peer = new RTCPeerConnection({
         iceServers: [
@@ -19,23 +21,26 @@ function createPeer() {
         ]
     });
     peer.ontrack = handleTrackEvent;
+
+    // need this event to be raised when we recieve an offer 
     peer.onnegotiationneeded = () => handleNegotiationNeededEvent(peer);
 
     return peer;
 }
-
+//create and offer:
 async function handleNegotiationNeededEvent(peer) {
     const offer = await peer.createOffer();
     await peer.setLocalDescription(offer);
     const payload = {
         sdp: peer.localDescription
     };
-
+    //when request for consumer is called send object to webrtc 
     const { data } = await axios.post('/consumer', payload);
     const desc = new RTCSessionDescription(data.sdp);
     peer.setRemoteDescription(desc).catch(e => console.log(e));
 }
 
+//append stream to video tag of this track
 function handleTrackEvent(e) {
     document.getElementById("video").srcObject = e.streams[0];
 };
