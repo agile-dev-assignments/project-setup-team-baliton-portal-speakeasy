f7e9439a847b554e6839ff74d433d180c153e621
Daniel Tomkovicz <dht253@nyu.edu>
2021-04-11 11:13:44 -0400

    finished styling about page

--- front-end/src/MainPage.js
+++ front-end/src/MainPage.js
@@ -7,6 +7,7 @@ const MainPage = (prop) => {                 //need  user name
   return (
     <div id="mainDiv">
       <div id="side">
+        <br></br>
         <h1 align = "left"> {prop.name} </h1>
         <Linker ali = "left" var = "primary" action = "/taglist" name = "Explore Tags" cn="sidebar"> </Linker>
         <Linker ali = "left" var = "primary" action = "/createcall" name = "Start Call" cn="sidebar"> </Linker>
@@ -15,6 +16,8 @@ const MainPage = (prop) => {                 //need  user name
       </div>
       <Textbox ali = "center" mess = "Enter Call Name: " action = "/chatroom" tag = "title" name = "Join Call" cn="callNameField"> </Textbox>
       <Linker ali = "center" var = "primary" action = "/chatroom" name = "I am feeling lucky!" cn = "buttons"> </Linker>
+      <br></br>
+      <br></br>
     </div>
   );
 }
